d45c804b1073e19940eac3773d71264d91120496
Hamasu Fumitaka <mutsuki.nakanogiwa@gmail.com>
2016-04-12 23:16:27 +0900

    v1.2.0

--- src/jquery.last-sidebar.js
+++ src/jquery.last-sidebar.js
@@ -1,74 +1,138 @@
 /*
- * jQuery Last Sidebar v1.0.0 - horizontal slide menu plugin
- * under the MIT license
+ * jQuery Last Sidebar v1.2.0 - horizontal slide menu plugin
  *
- * Written while knead boobs in one hand
- * Copyright 2015 Suzusaki Makoto - http://xn--nckmepf1g6g.com/
+ * under the MIT license
+ * http://opensource.org/licenses/MIT
+ *
+ * Written while knead boobs in one hand by Suzusaki Makoto
+ * http://xn--nckmepf1g6g.com/
  */
 
 (function($){
-	
+
 	$.fn.lastSidebar = function(options){
-		
+
+		// default settings
 		var defaults = {
 			itemTrigger: '#menuTrigger',
 			align: 'right',
 			speed: 300,
-			breakPoint: false
+			breakPoint: false,
+			noscroll: false,
+			openBefore: function(){},
+			openAfter: function(){},
+			closeBefore: function(){},
+			closeAfter: function(){}
 		};
+
+		// merge the default
 		var params = $.extend(defaults, options);
 		var menu = this;
-		var menuWidth;
-		var docBody;
-		
+		var docBody = $(document.body);
+		var currentScrollY;
+
+		// is opend
+		var opend = true;
+
+		// ------------------------------
+		// load functions
+		//------------------------------
+
+		// add class name
 		menu.addClass('lastSidebar');
-		$('body').append('<a href="#" id="lastOverlay"></a>');
+
+		// add overlay
+		var overlayName = '#lastOverlay';
+		var overlay = document.createElement('a');
+		overlay.setAttribute('href', '#');
+		overlay.id = 'lastOverlay';
+		document.body.appendChild(overlay);
+
+		// resize position
 		resizePosition();
-		$(window).on('resize', function(){
-			docBody = $(document.body);
+
+		// ------------------------------
+		// event
+		//------------------------------
+
+		// eventName define
+		var eventResize;
+		var eventTap;
+		if((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') === -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0){
+			eventResize = 'orientationchange';
+			eventTap = 'tap click';
+		}else{
+			eventResize = 'resize';
+			eventTap = 'click';
+		}
+
+		// resize functions
+		$(window).on(eventResize, resizeAction);
+
+		// tap functions
+		$(params.itemTrigger + ', ' + overlayName).on(eventTap, tapAction);
+
+		// if noscroll ( for iOS7- )
+		$(window).on('scroll', function(){
+			if($('body').hasClass('noscroll')){
+				$('html').height(window.innerHeight);
+			}
+		});
+
+		// resize action
+		function resizeAction(){
 			if(docBody.hasClass('open')){
 				docBody.css(params.align, 0).removeClass('open');
 				$(params.itemTrigger).removeClass('active');
-				$('#lastOverlay').hide();
+				$(overlayName).hide();
+			}
+			if(docBody.hasClass('noscroll')){
+				var currentBody = -1 * parseInt(docBody.css('top'));
+				docBody.removeClass('noscroll').scrollTop(currentBody).css('top', '');
+				$('html').height('');
 			}
 			resizePosition();
-		});
-		
-		$(params.itemTrigger + ', #lastOverlay').on('tap', function(){
-			tapAction();
-			return false;
-		});
-		
+		}
+
+		// resize positioning
 		function resizePosition(){
-			if(!params.breakPoint || window.innerWidth <= params.breakPoint){
-				switch(params.align){
-					case 'right':
-						menu.css({ left: 'auto', right: -menu.width() }).hide();
-						break;
-					case 'left':
-						menu.css({ left: -menu.width(), right: 'auto' }).hide();
-						break;
+			if(opend){
+				var prptLeft,
+						prptRight;
+				// No set of break point or Window width is mobile
+				if(!params.breakPoint || window.innerWidth <= params.breakPoint){
+					switch(params.align){
+						case 'right':
+							prptLeft = 'auto';
+							prptRight = -menu.width();
+							break;
+						case 'left':
+							prptLeft = -menu.width();
+							prptRight = 'auto';
+							break;
+						// no default route
+					}
+					menu.css({ left: prptLeft, right: prptRight }).hide();
+				}else{
+					menu.css({ left: '', right: '' });
+					$(overlayName).hide();
 				}
-			}else{
-				switch(params.align){
-					case 'right':
-						menu.css({ left: '', right: '' });
-						break;
-					case 'left':
-						menu.css({ left: '', right: '' });
-						break;
-				}
-				$('#lastOverlay').hide();
+				opend = false;
 			}
 		}
-		
+
+		// tap action
 		function tapAction(){
-			docBody = $(document.body);
-			menuWidth = menu.outerWidth();
+			var menuWidth = menu.outerWidth();
 			docBody.toggleClass('open');
+			// No set of break point or Window width is mobile
 			if(!params.breakPoint || window.innerWidth <= params.breakPoint){
 				if(docBody.hasClass('open')){
-					$('#lastOverlay').stop().fadeIn(params.speed);
+					// callback
+					params.openBefore();
+					// show overlay
+					$(overlayName).stop().fadeIn(params.speed);
+					// show in the slide
 					switch(params.align){
 						case 'right':
 							menu.stop().show().animate({ right: 0 }, params.speed, 'swing');
@@ -78,10 +142,23 @@
 							menu.stop().show().animate({ left: 0 }, params.speed, 'swing');
 							docBody.stop().animate({ left: menuWidth }, params.speed, 'swing');
 							break;
+						// no default route
 					}
 					$(params.itemTrigger).addClass('active');
+					if(params.noscroll){
+						$('html').height(window.innerHeight);
+						currentScrollY = $(window).scrollTop();
+						docBody.addClass('noscroll').css('top', -1 * currentScrollY);
+					}
+					opend = true;
+					// callback
+					params.openAfter();
 				}else{
-					$('#lastOverlay').stop().fadeOut(params.speed);
+					// callback
+					params.closeBefore();
+					// hide overlay
+					$(overlayName).stop().fadeOut(params.speed);
+					// hide in the slide
 					switch(params.align){
 						case 'right':
 							menu.stop().animate({ right: -menuWidth }, params.speed, 'swing', function(){
@@ -95,14 +172,24 @@
 							});
 							docBody.stop().animate({ left: 0 }, params.speed, 'swing');
 							break;
+						// no default route
 					}
 					$(params.itemTrigger).removeClass('active');
+					if(params.noscroll){
+						$('html').height('');
+						docBody.removeClass('noscroll').css('top', '');
+						$(window).scrollTop(currentScrollY);
+					}
+					opend = false;
+					// callback
+					params.closeAfter();
 				}
 			}
+			return false;
 		}
-		
+
 		return(this);
-		
+
 	};
-	
+
 })(jQuery);

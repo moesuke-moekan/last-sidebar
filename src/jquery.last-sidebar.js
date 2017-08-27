452c29c6ddc45db665c7948a2c65604ac36ef383
Hamasu Fumitaka <mutsuki.nakanogiwa@gmail.com>
2017-08-27 14:54:20 +0900

    [clean] リファクタリング

--- src/jquery.last-sidebar.js
+++ src/jquery.last-sidebar.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Last Sidebar v1.2.0 - horizontal slide menu plugin
+ * jQuery Last Sidebar v1.2.1 - horizontal slide menu plugin
  *
  * under the MIT license
  * http://opensource.org/licenses/MIT
@@ -10,7 +10,7 @@
 
 (function($){
 
-	$.fn.lastSidebar = function(options){
+	$.fn.lastSidebar = function(options) {
 
 		// default settings
 		var defaults = {
@@ -25,80 +25,97 @@
 			closeAfter: function(){}
 		};
 
+		// this
+		var menu = this;
+
 		// merge the default
 		var params = $.extend(defaults, options);
-		var menu = this;
-		var docBody = $(document.body);
+		var $docBody = $(document.body);
 		var currentScrollY;
 
-		// is opend
-		var opend = true;
+		// overlay
+		var overlayIdName = 'lastOverlay';
+		var $overlay = null;
+
+		// is open
+		var isOpen = true;
 
 		// ------------------------------
 		// load functions
-		//------------------------------
+		// ------------------------------
 
-		// add class name
-		menu.addClass('lastSidebar');
+		var init = function() {
 
-		// add overlay
-		var overlayName = '#lastOverlay';
-		var overlay = document.createElement('a');
-		overlay.setAttribute('href', '#');
-		overlay.id = 'lastOverlay';
-		document.body.appendChild(overlay);
+			// add class name
+			menu.addClass('lastSidebar');
 
-		// resize position
-		resizePosition();
+			// add overlay
+			var overlay = document.createElement('a');
+			overlay.setAttribute('href', '#');
+			overlay.id = overlayIdName;
+			document.body.appendChild(overlay);
+			$overlay = $('#' + overlayIdName);
+
+			// resize position
+			resizePosition();
+
+			// ------------------------------
+			// event
+			// ------------------------------
+
+			// eventName define
+			var eventResize;
+			var eventTap;
+			var ua = navigator.userAgent;
+			if((ua.indexOf('iPhone') > 0 && ua.indexOf('iPad') === -1) || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0) {
+				eventResize = 'orientationchange';
+				eventTap = 'tap click';
+			} else {
+				eventResize = 'resize';
+				eventTap = 'click';
+			}
+
+			// resize functions
+			$(window).on(eventResize, resizeAction);
+
+			// tap functions
+			$(params.itemTrigger).add($overlay).on(eventTap, tapAction);
+
+			// if noscroll ( for iOS7- )
+			$(window).on('scroll', function() {
+				if($('body').hasClass('noscroll')){
+					$('html').height(window.innerHeight);
+				}
+			});
+		};
 
 		// ------------------------------
-		// event
-		//------------------------------
-
-		// eventName define
-		var eventResize;
-		var eventTap;
-		if((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') === -1) || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0){
-			eventResize = 'orientationchange';
-			eventTap = 'tap click';
-		}else{
-			eventResize = 'resize';
-			eventTap = 'click';
-		}
-
-		// resize functions
-		$(window).on(eventResize, resizeAction);
-
-		// tap functions
-		$(params.itemTrigger + ', ' + overlayName).on(eventTap, tapAction);
-
-		// if noscroll ( for iOS7- )
-		$(window).on('scroll', function(){
-			if($('body').hasClass('noscroll')){
-				$('html').height(window.innerHeight);
-			}
-		});
-
 		// resize action
-		function resizeAction(){
-			if(docBody.hasClass('open')){
-				docBody.css(params.align, 0).removeClass('open');
+		// ------------------------------
+
+		var resizeAction = function() {
+			if($docBody.hasClass('open')){
+				$docBody.css(params.align, 0).removeClass('open');
 				$(params.itemTrigger).removeClass('active');
-				$(overlayName).hide();
+				$overlay.hide();
 			}
-			if(docBody.hasClass('noscroll')){
-				var currentBody = -1 * parseInt(docBody.css('top'));
-				docBody.removeClass('noscroll').scrollTop(currentBody).css('top', '');
+			if($docBody.hasClass('noscroll')){
+				var currentBody = parseInt($docBody.css('top'), 10) * -1;
+				$docBody.removeClass('noscroll').scrollTop(currentBody).css('top', '');
 				$('html').height('');
 			}
 			resizePosition();
-		}
+		};
 
+
+		// ------------------------------
 		// resize positioning
-		function resizePosition(){
-			if(opend){
+		// ------------------------------
+
+		var resizePosition = function() {
+			if(isOpen){
 				var prptLeft,
-						prptRight;
+					prptRight;
 				// No set of break point or Window width is mobile
 				if(!params.breakPoint || window.innerWidth <= params.breakPoint){
 					switch(params.align){
@@ -110,83 +127,97 @@
 							prptLeft = -menu.width();
 							prptRight = 'auto';
 							break;
-						// no default route
+						default:
+							// no default route
+							break;
 					}
 					menu.css({ left: prptLeft, right: prptRight }).hide();
 				}else{
 					menu.css({ left: '', right: '' });
-					$(overlayName).hide();
+					$overlay.hide();
 				}
-				opend = false;
+				isOpen = false;
 			}
-		}
+		};
 
+
+		// ------------------------------
 		// tap action
-		function tapAction(){
+		// ------------------------------
+
+		var tapAction = function() {
+			console.log('!');
 			var menuWidth = menu.outerWidth();
-			docBody.toggleClass('open');
+			$docBody.toggleClass('open');
 			// No set of break point or Window width is mobile
 			if(!params.breakPoint || window.innerWidth <= params.breakPoint){
-				if(docBody.hasClass('open')){
+				if($docBody.hasClass('open')){
 					// callback
 					params.openBefore();
 					// show overlay
-					$(overlayName).stop().fadeIn(params.speed);
+					$overlay.stop().fadeIn(params.speed);
 					// show in the slide
 					switch(params.align){
 						case 'right':
 							menu.stop().show().animate({ right: 0 }, params.speed, 'swing');
-							docBody.stop().animate({ right: menuWidth }, params.speed, 'swing');
+							$docBody.stop().animate({ right: menuWidth }, params.speed, 'swing');
 							break;
 						case 'left':
 							menu.stop().show().animate({ left: 0 }, params.speed, 'swing');
-							docBody.stop().animate({ left: menuWidth }, params.speed, 'swing');
+							$docBody.stop().animate({ left: menuWidth }, params.speed, 'swing');
 							break;
-						// no default route
+						default:
+							// no default route
+							break;
 					}
 					$(params.itemTrigger).addClass('active');
 					if(params.noscroll){
 						$('html').height(window.innerHeight);
 						currentScrollY = $(window).scrollTop();
-						docBody.addClass('noscroll').css('top', -1 * currentScrollY);
+						$docBody.addClass('noscroll').css('top', -1 * currentScrollY);
 					}
-					opend = true;
+					isOpen = true;
 					// callback
 					params.openAfter();
 				}else{
 					// callback
 					params.closeBefore();
 					// hide overlay
-					$(overlayName).stop().fadeOut(params.speed);
+					$overlay.stop().fadeOut(params.speed);
 					// hide in the slide
 					switch(params.align){
 						case 'right':
 							menu.stop().animate({ right: -menuWidth }, params.speed, 'swing', function(){
 								menu.hide();
 							});
-							docBody.stop().animate({ right: 0 }, params.speed, 'swing');
+							$docBody.stop().animate({ right: 0 }, params.speed, 'swing');
 							break;
 						case 'left':
 							menu.stop().animate({ left: -menuWidth }, params.speed, 'swing', function(){
 								menu.hide();
 							});
-							docBody.stop().animate({ left: 0 }, params.speed, 'swing');
+							$docBody.stop().animate({ left: 0 }, params.speed, 'swing');
 							break;
-						// no default route
+						default:
+							// no default route
+							break;
 					}
 					$(params.itemTrigger).removeClass('active');
 					if(params.noscroll){
 						$('html').height('');
-						docBody.removeClass('noscroll').css('top', '');
+						$docBody.removeClass('noscroll').css('top', '');
 						$(window).scrollTop(currentScrollY);
 					}
-					opend = false;
+					isOpen = false;
 					// callback
 					params.closeAfter();
 				}
 			}
 			return false;
-		}
+		};
+
+		// start initialize
+		init();
 
 		return(this);
 

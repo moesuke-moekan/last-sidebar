7bd6b5c511454924de84f9833bc302df54ab11dd
Hamasu Fumitaka <mutsuki.nakanogiwa@gmail.com>
2017-11-23 16:21:37 +0900

    [add] 外から閉じるためのメソッドを追加

--- src/jquery.last-sidebar.js
+++ src/jquery.last-sidebar.js
@@ -1,14 +1,14 @@
 /*
- * jQuery Last Sidebar v1.2.1 - horizontal slide menu plugin
+ * jQuery Last Sidebar v1.3.0 - horizontal slide menu plugin
  *
  * under the MIT license
  * http://opensource.org/licenses/MIT
  *
  * Written while knead boobs in one hand by Suzusaki Makoto
- * http://xn--nckmepf1g6g.com/
+ * https://xn--nckmepf1g6g.com/
  */
 
-(function($){
+(function($) {
 
 	$.fn.lastSidebar = function(options) {
 
@@ -17,7 +17,7 @@
 			itemTrigger: '#menuTrigger',
 			align: 'right',
 			speed: 300,
-			breakPoint: false,
+			breakPoint: null,
 			noscroll: false,
 			openBefore: function(){},
 			openAfter: function(){},
@@ -33,21 +33,35 @@
 		var $docBody = $(document.body);
 		var currentScrollY;
 
+		// validation
+		if(params.breakPoint !== null) {
+			if(isNaN(params.breakPoint)) {
+				params.breakPoint = null;
+			} else if(typeof params.breakPoint !== Number) {
+				params.breakPoint = Number(params.breakPoint);
+			}
+		}
+
+		// button
+		var $itemTrigger = null;
+
 		// overlay
 		var overlayIdName = 'lastOverlay';
 		var $overlay = null;
 
 		// is open
-		var isOpen = true;
+		var isOpen = false;
 
-		// ------------------------------
-		// load functions
-		// ------------------------------
-
+		/**
+		 * load functions
+		 */
 		var init = function() {
 
 			// add class name
 			menu.addClass('lastSidebar');
+
+			// button
+			$itemTrigger = $(params.itemTrigger);
 
 			// add overlay
 			var overlay = document.createElement('a');
@@ -57,7 +71,7 @@
 			$overlay = $('#' + overlayIdName);
 
 			// resize position
-			resizePosition();
+			resizePosition(true);
 
 			// ------------------------------
 			// event
@@ -79,147 +93,200 @@
 			$(window).on(eventResize, resizeAction);
 
 			// tap functions
-			$(params.itemTrigger).add($overlay).on(eventTap, tapAction);
-
-			// if noscroll ( for iOS7- )
-			$(window).on('scroll', function() {
-				if($('body').hasClass('noscroll')){
-					$('html').height(window.innerHeight);
-				}
-			});
+			$itemTrigger.add($overlay).on(eventTap, tapAction);
 		};
 
-		// ------------------------------
-		// resize action
-		// ------------------------------
-
+		/**
+		 * resize action
+		 */
 		var resizeAction = function() {
-			if($docBody.hasClass('open')){
+			if($docBody.hasClass('open')) {
 				$docBody.css(params.align, 0).removeClass('open');
-				$(params.itemTrigger).removeClass('active');
+				$itemTrigger.removeClass('active');
 				$overlay.hide();
 			}
-			if($docBody.hasClass('noscroll')){
+			if($docBody.hasClass('noscroll')) {
 				var currentBody = parseInt($docBody.css('top'), 10) * -1;
 				$docBody.removeClass('noscroll').scrollTop(currentBody).css('top', '');
 				$('html').height('');
 			}
-			resizePosition();
+			resizePosition(false);
 		};
 
-
-		// ------------------------------
-		// resize positioning
-		// ------------------------------
-
-		var resizePosition = function() {
-			if(isOpen){
-				var prptLeft,
-					prptRight;
-				// No set of break point or Window width is mobile
-				if(!params.breakPoint || window.innerWidth <= params.breakPoint){
-					switch(params.align){
-						case 'right':
-							prptLeft = 'auto';
-							prptRight = -menu.width();
-							break;
-						case 'left':
-							prptLeft = -menu.width();
-							prptRight = 'auto';
-							break;
-						default:
-							// no default route
-							break;
-					}
-					menu.css({ left: prptLeft, right: prptRight }).hide();
-				}else{
-					menu.css({ left: '', right: '' });
-					$overlay.hide();
-				}
-				isOpen = false;
+		/**
+		 * resize positioning
+		 * @param isFirstLoad
+		 */
+		var resizePosition = function(isFirstLoad) {
+			if(!(isOpen || isFirstLoad)) {
+				return;
 			}
-		};
-
-
-		// ------------------------------
-		// tap action
-		// ------------------------------
-
-		var tapAction = function() {
-			var menuWidth = menu.outerWidth();
-			$docBody.toggleClass('open');
+			var propLeft,
+				propRight;
 			// No set of break point or Window width is mobile
-			if(!params.breakPoint || window.innerWidth <= params.breakPoint){
+			if(!params.breakPoint || window.innerWidth <= params.breakPoint || isFirstLoad) {
+				switch(params.align){
+					case 'right':
+						propLeft = 'auto';
+						propRight = menu.width() * -1;
+						break;
+					case 'left':
+						propLeft = menu.width() * -1;
+						propRight = 'auto';
+						break;
+					default:
+						// no default route
+						break;
+				}
+				menu.css({
+					left: propLeft,
+					right: propRight
+				}).hide();
+			} else {
+				menu.css({
+					left: '',
+					right: ''
+				});
+				$overlay.hide();
+			}
+			isOpen = false;
+		};
+
+		/**
+		 * tap action
+		 */
+		var tapAction = function(ev) {
+			ev.preventDefault();
+
+			var menuWidth = menu.outerWidth();
+			// No set of break point or Window width is mobile
+			if(!params.breakPoint || window.innerWidth <= params.breakPoint) {
+				$docBody.toggleClass('open');
 				if($docBody.hasClass('open')){
-					// callback
-					params.openBefore();
-					// show overlay
-					$overlay.stop().fadeIn(params.speed);
-					// show in the slide
-					switch(params.align){
-						case 'right':
-							menu.stop().show().animate({ right: 0 }, params.speed, 'swing');
-							$docBody.stop().animate({ right: menuWidth }, params.speed, 'swing');
-							break;
-						case 'left':
-							menu.stop().show().animate({ left: 0 }, params.speed, 'swing');
-							$docBody.stop().animate({ left: menuWidth }, params.speed, 'swing');
-							break;
-						default:
-							// no default route
-							break;
-					}
-					$(params.itemTrigger).addClass('active');
-					if(params.noscroll){
-						$('html').height(window.innerHeight);
-						currentScrollY = $(window).scrollTop();
-						$docBody.addClass('noscroll').css('top', -1 * currentScrollY);
-					}
-					isOpen = true;
-					// callback
-					params.openAfter();
-				}else{
-					// callback
-					params.closeBefore();
-					// hide overlay
-					$overlay.stop().fadeOut(params.speed);
-					// hide in the slide
-					switch(params.align){
-						case 'right':
-							menu.stop().animate({ right: -menuWidth }, params.speed, 'swing', function(){
-								menu.hide();
-							});
-							$docBody.stop().animate({ right: 0 }, params.speed, 'swing');
-							break;
-						case 'left':
-							menu.stop().animate({ left: -menuWidth }, params.speed, 'swing', function(){
-								menu.hide();
-							});
-							$docBody.stop().animate({ left: 0 }, params.speed, 'swing');
-							break;
-						default:
-							// no default route
-							break;
-					}
-					$(params.itemTrigger).removeClass('active');
-					if(params.noscroll){
-						$('html').height('');
-						$docBody.removeClass('noscroll').css('top', '');
-						$(window).scrollTop(currentScrollY);
-					}
-					isOpen = false;
-					// callback
-					params.closeAfter();
+					openAction(menuWidth);
+				} else {
+					closeAction(menuWidth);
 				}
 			}
-			return false;
+		};
+
+		/**
+		 * open sidebar
+		 * @param menuWidth
+		 */
+		var openAction = function(menuWidth) {
+			// callback
+			params.openBefore();
+
+			// show overlay
+			$overlay.stop().fadeIn(params.speed);
+
+			// show in the slide
+			switch(params.align){
+				case 'right':
+					menu.stop().show().animate({
+						right: 0
+					}, params.speed, 'swing');
+					$docBody.stop().animate({
+						right: menuWidth
+					}, params.speed, 'swing');
+					break;
+				case 'left':
+					menu.stop().show().animate({
+						left: 0
+					}, params.speed, 'swing');
+					$docBody.stop().animate({
+						left: menuWidth
+					}, params.speed, 'swing');
+					break;
+				default:
+					// no default route
+					break;
+			}
+			$itemTrigger.addClass('active');
+			if(params.noscroll){
+				$('html').height(window.innerHeight);
+				currentScrollY = $(window).scrollTop();
+				$docBody.addClass('noscroll').css('top', -1 * currentScrollY);
+			}
+			isOpen = true;
+
+			// callback
+			params.openAfter();
+		};
+
+		/**
+		 * close sidebar
+		 * @param menuWidth
+		 */
+		var closeAction = function(menuWidth) {
+			// callback
+			params.closeBefore();
+
+			// hide overlay
+			$overlay.stop().fadeOut(params.speed);
+
+			// hide in the slide
+			switch(params.align){
+				case 'right':
+					menu.stop().animate({
+						right: (menuWidth * -1)
+					}, params.speed, 'swing', function() {
+						menu.hide();
+					});
+					$docBody.stop().animate({
+						right: 0
+					}, params.speed, 'swing');
+					break;
+				case 'left':
+					menu.stop().animate({
+						left: (menuWidth * -1)
+					}, params.speed, 'swing', function() {
+						menu.hide();
+					});
+					$docBody.stop().animate({
+						left: 0
+					}, params.speed, 'swing');
+					break;
+				default:
+					// no default route
+					break;
+			}
+			$itemTrigger.removeClass('active');
+			if(params.noscroll){
+				$('html').height('');
+				$docBody.removeClass('noscroll').css('top', '');
+				$(window).scrollTop(currentScrollY);
+			}
+			isOpen = false;
+
+			// callback
+			params.closeAfter();
+		};
+
+		// ------------------------------
+		// PUBLIC FUNCTIONS
+		// ------------------------------
+
+		/**
+		 * close sidebar
+		 */
+		menu.closeSlide = function() {
+			// No set of break point or Window width is mobile
+			if(!params.breakPoint || window.innerWidth <= params.breakPoint) {
+				if(!$docBody.hasClass('open')){
+					return;
+				}
+				var menuWidth = menu.outerWidth();
+				$docBody.removeClass('open');
+				closeAction(menuWidth);
+			}
 		};
 
 		// start initialize
 		init();
 
-		return(this);
-
+		return this;
 	};
 
 })(jQuery);
